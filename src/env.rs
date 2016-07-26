25bf0d95f861726d3f8e44dfb6eca582bcb8a9f5
zjhmale <zjhmale@gmail.com>
2016-07-26 13:08:44 +0800

    add assumptions

--- src/env.rs
+++ src/env.rs
@@ -24,3 +24,20 @@ impl TypeVars<HashMap<&'static str, Scheme>> for HashMap<&'static str, Scheme> {
         new_env
     }
 }
+
+pub fn get_assumptions() -> HashMap<&'static str, Scheme> {
+    let mut assumptions: HashMap<&'static str, Scheme> = HashMap::new();
+    assumptions.insert("zero", Mono(Box::new(TInt)));
+    assumptions.insert("one", Mono(Box::new(TInt)));
+    assumptions.insert("true", Mono(Box::new(TBool)));
+    assumptions.insert("false", Mono(Box::new(TBool)));
+    assumptions.insert("not", Mono(Box::new(TArrow(Box::new(TBool), Box::new(TBool)))));
+    assumptions.insert("and", Mono(Box::new(TArrow(Box::new(TBool), Box::new(TArrow(Box::new(TBool), Box::new(TBool)))))));
+    assumptions.insert("add", Mono(Box::new(TArrow(Box::new(TInt), Box::new(TArrow(Box::new(TInt), Box::new(TInt)))))));
+    assumptions.insert("id", Poly("a", Box::new(Mono(Box::new(TArrow(Box::new(TVar("a")), Box::new(TVar("a"))))))));
+    assumptions.insert("eq", Poly("a", Box::new(Mono(Box::new(TArrow(Box::new(TVar("a")), Box::new(TArrow(Box::new(TVar("a")), Box::new(TBool)))))))));
+    assumptions.insert("compose", Poly("a", Box::new(Poly("b", Box::new(Poly("c", Box::new(Mono(Box::new(TArrow(Box::new(TArrow(Box::new(TVar("b")), Box::new(TVar("c")))), Box::new(TArrow(Box::new(TArrow(Box::new(TVar("a")), Box::new(TVar("b")))), Box::new(TArrow(Box::new(TVar("a")), Box::new(TVar("c"))))))))))))))));
+    assumptions.insert("choose", Poly("a", Box::new(Mono(Box::new(TArrow(Box::new(TVar("a")), Box::new(TArrow(Box::new(TVar("a")), Box::new(TVar("a"))))))))));
+    assumptions.insert("wrong", Poly("a", Box::new(Mono(Box::new(TVar("a"))))));
+    assumptions
+}
